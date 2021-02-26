8d65a221d630b0429eb6a2cfcb759adfe3835c19
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-02-27 02:45:09 +0900

    chapter2-01

---
+++ Chapter2/01.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int val = 7;
+    boolean flg = true;
+    if (flg == true) {
+      do {
+        System.out.println(val);
+      } while (val > 10);
+    }
+  }
+}
