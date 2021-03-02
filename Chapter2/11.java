4e51c6b166f42e7fe6988a699497aba7f18a6b1e
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-02 23:08:33 +0900

    Chapter2

---
+++ Chapter2/11.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String str = "abcde";
+    System.out.println(str.charAt(4));
+    // charAt => 文字列から特定の一文字だけ抜き出す
+  }
+}
---
+++ Chapter2/12.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String str = "abcde";
+    System.out.println(str.indexOf("abcdef"));
+    // indexOf => 引数に指定した文字列が含まれる順番を返す。含まれない場合は -1 が返る。
+  }
+}
