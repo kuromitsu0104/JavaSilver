cae1afd4c9cdf7faf158adf07569dff033baee23
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-03 00:49:48 +0900

    Chapter2 / 17..20

---
+++ Chapter2/17.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    String str = "Hello, ".concat("Java!");
+    System.out.println(str);
+    // concat => Stringクラスのメソッド, 文字列を連結する。
+  }
+}
---
+++ Chapter2/18.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(10 + 20 + "30" + 40);
+    // 数字と文字列の結合の場合は、文字列の結合として処理される。
+  }
+}
