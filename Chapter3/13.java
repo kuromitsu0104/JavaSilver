7346bb0128a8747046014511d9c0109a614cfe82
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-06 15:22:17 +0900

    Chapter3 / 13

---
+++ Chapter3/13.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    String a = new String("sample");
+    String b = "sample";
+    System.out.print(a == b);
+    System.out.print(", ");
+    System.out.println(a.equals(b));
+  }
+}
+// 文字列リテラルの場合はコンスタントプールにより同一参照となる。
+// new演算子では明示的に新しいインスタンスを作成するため、参照元が異なりfalseとなる。
