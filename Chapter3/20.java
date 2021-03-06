d6dcfa4e5e25df201043a3482a3cfc04297a49ac
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-06 16:24:14 +0900

    Chapter3 / 14..21

---
+++ Chapter3/20.java
@@ -0,0 +1,20 @@
+public class Main {
+  public static void main(String[] args) {
+    final int NUM = 0;
+    int num = 10;
+    switch (num) {
+      // case "10":
+      //   System.out.println("A");
+      //   break;
+      // case num:
+      //   System.out.println("B");
+      //   break;
+      case 2 * 5:
+        System.out.println("C");
+        break;
+      case NUM:
+        System.out.println("D");
+        break;
+    }
+  }
+}
---
+++ Chapter3/21.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(Stringp[] args) {
+    int num = 1;
+    switch (num) {
+      case 1:
+      case 2:
+      case 3: System.out.println("A");
+      case 4: System.out.println("B");
+      default:
+        System.out.println("C");
+    }
+  }
+}
