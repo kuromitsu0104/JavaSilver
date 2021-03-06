b1fd923d8fb7a796d4b14f232ce229b9c8b83d51
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-03-06 16:49:31 +0900

    Chapter4 / 01..03

---
+++ Chapter4/02.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int a = 0;
+    do {
+      System.out.println(a++);
+    } while (a < 5);
+  }
+}
+// while文, do-while文で条件判定するタイミングの違いに注意
---
+++ Chapter4/03.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int a = 0;
+    while (a < 5)
+      do {
+        a++;
+        System.out.print(a);
+      } while (a < 5);
+  }
+}
+// 省略記法の場合は1文のみ。
+// 2文以上実行する場合は{}が必要。
